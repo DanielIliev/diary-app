40fcb2c5779edd8f8d54466c61015dffcf362cff
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-01-02 22:04:09 +0200

    Listing tasks + some styling on the elements

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+  data = [
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+  ];
+}
---
+++ src/app/navigation/navig/navig.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navig',
+  templateUrl: './navig.component.html',
+  styleUrls: ['./navig.component.scss']
+})
+export class NavigComponent {
+  pesho() {
+    console.log('Das shnitzel');
+  }
+}
