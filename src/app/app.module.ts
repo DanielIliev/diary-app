6e9a696b3aefaa9e4b22c3c7f563d9d8ff037be2
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-01-03 03:15:48 +0200

    Updated the navigation / new components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavigComponent } from './navigation/navig/navig.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    NavigComponent
+    NavigComponent,
+    HomeComponent,
+    NotesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  data = [
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+    {
+      title: 'Take out the trash',
+      content: 'I need to throw out the trash',
+      dueTo: 'Friday',
+      createdOn: 'Monday',
+      priority: 'low-priority',
+    },
+  ];
+}
