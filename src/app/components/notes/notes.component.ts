ea23a15abddb1729d79aa0e560a6ceb7f5f56191
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-01-10 22:46:48 +0200

    note modal redesigned

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -18,7 +18,7 @@ export class NotesComponent {
 
   constructor(private dialog: MatDialog) {
     for (let index = 0; index < 10; index++) {
-      this.notes.push(this.generateDummyNotes('Dummy title', 'This is the dummy content', 'Monday', new Date().toString(), 'medium'));
+      this.notes.push(this.generateDummyNotes('Dummy title', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Monday', new Date().toString(), 'medium'));
     }
     this.notes.push(
       {
